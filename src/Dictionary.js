76b95e9afa090a3877760b7e070bb403ff68a5eb
erinantonia <145416233+erinantonia@users.noreply.github.com>
2024-07-04 17:23:40 +1200

    Edited search function

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,14 +8,12 @@ export default function Dictionary() {
     console.log(response.data[0]);
   }
 
-  let key = "36314taf33204213d293afod0a3b00b2";
-  let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
-  axios.get(apiUrl).then(handleResponse);
-
   function search(event) {
     event.preventDefault();
-    console.log(event);
-    alert(`Searching for ${keyword}`);
+
+    let key = "36314taf33204213d293afod0a3b00b2";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
